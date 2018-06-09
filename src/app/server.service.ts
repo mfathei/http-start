afc26627c3e9d16718fc97bce1310e419d29699a
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-06-09 06:26:48 +0200

    234 Adjusting request headers

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http'
+import { Http, Headers } from '@angular/http'
 
 @Injectable()
 export class ServerService{
     
     constructor(private http: Http){}
-    
+
     saveServers(servers: any[]){
-        return this.http.post('https://http-start-8f266.firebaseio.com/data.json', servers);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('https://http-start-8f266.firebaseio.com/data.json', servers, {
+            headers: headers
+        });
     }
 }
